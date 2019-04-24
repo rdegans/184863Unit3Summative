03bb13390f224b8bdbd4aeb2ce9a6fbd6778e76b
rdegans <38084642+rdegans@users.noreply.github.com>
2019-04-24 11:17:25 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Name: Riley de Gans
+ * Date: April 22nd, 2019
+ * Description:  A program that cheats at scrabble using an online dictionary and some inefficient logic.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,186 +24,100 @@ namespace _184863Unit3Summative
 {
     public partial class MainWindow : Window
     {
-        string currentWord, tempWord, lastWord, tiles;
-
-        private void BtnPlay_Click(object sender, RoutedEventArgs e)
-        {
-            tiles = txtInput.Text;
-            MessageBox.Show(tiles, "Your Tiles:");
-            while (tiles.IndexOf(" ") != -1)
-            {
-                tiles = tiles.Remove(tiles.IndexOf(" "), 1);
-            }
-            int blankTiles = 7 - tiles.Length;
-            System.Net.WebClient wc = new System.Net.WebClient();
-            Stream download = wc.OpenRead("http://darcy.rsgc.on.ca/ACES/ICS4U/SourceCode/Words.txt");
-            StreamReader sr = new StreamReader(download);
-            StreamWriter sw = new StreamWriter("scoringWords.txt");
-            for (int i = 0; i < tiles.Length; i++)
-            {
-                int removeLetter = badLetters.IndexOf(tiles.Substring(i, 1));
-                if (removeLetter != -1)
-                {
-                    badLetters = badLetters.Remove(removeLetter, 1);
-                }
-            }
-            while (!sr.EndOfStream)
-            {
-                int counter = 0;
-                currentWord = sr.ReadLine().ToUpper();
-                tempWord = currentWord;
-                if (currentWord.Length < 8 && currentWord != lastWord)
-                {
-                    for (int i = 0; i < badLetters.Length; i++)
-                    {
-                        if (currentWord.Contains(badLetters.Substring(i, 1)))
-                        {
-                            counter++;
-                        }
-                        if (counter > blankTiles)
-                        {
-                            i = 26;
-                        }
-                    }
-                    if (counter <= blankTiles)
-                    {
-                        for (int i = 0; i < tiles.Length; i++)
-                        {
-                            string currentLetter = tiles.Substring(i, 1);
-                            int removeLetter = tempWord.IndexOf(currentLetter);
-                            if (removeLetter > -1)
-                            {
-                                tempWord = tempWord.Remove(removeLetter, 1);
-                            }
-                        }
-                        if (tempWord.Length == blankTiles || tempWord.Length == 0)
-                        {
-                            sw.WriteLine(currentWord);
-                            lastWord = currentWord;
-                        }
-                    }
-                }
-            }
-            sr.Close();
-            sw.Close();
-            StreamReader sr2 = new StreamReader("scoringWords.txt");
-            string tempString = "";
-            int maxPoints = 0;
-            string topWord = "";
-            while (!sr2.EndOfStream)
-            {
-                currentWord = sr2.ReadLine();
-                int totalPoints = 0;
-                for (int i = 0; i < currentWord.Length; i++)
-                {
-                    if (!badLetters.Contains(currentWord.ToCharArray()[i]))
-                    {
-                        ScrabbleLetter sl = new ScrabbleLetter(currentWord.ToCharArray()[i]);
-                        totalPoints += sl.Points;
-                    }
-                }
-                if (totalPoints > maxPoints)
-                {
-                    maxPoints = totalPoints;
-                    topWord = currentWord;
-                }
-                tempString += currentWord + " ";
-            }
-            txtOutput.Text = "Scoring Words:\n\n" + tempString + "\n\nHigh Scoring Word is " + topWord.Substring(0, 1) + topWord.Substring(1).ToLower() + " with a score of " + maxPoints + "\n\n" + "Your Tiles:\n\n" + tiles;
-        }
-
-        string badLetters = "ETAOINSHRDLCUMWFGYPBVKJXQZ";//SORT FROM MOST COMMON TO LEAST
+        string currentWord, tempWord, lastWord, tiles, tempString, topWord, scoringLetters; //Declare string variables
+        int maxPoints = 0;//Declare int variables
+        string badLetters = "ETAOINSHRDLCUMWFGYPBVKJXQZ";//THe alphabet sorted by frequency in the english language to reduce run time by searching for the most common restricted letters
         public MainWindow()
         {
             InitializeComponent();
-            ScrabbleGame sg = new ScrabbleGame();
+            ScrabbleGame sg = new ScrabbleGame();//Create the scrabble game class and draw your tiles
             tiles = sg.drawInitialTiles();
+            tiles = "DSOPPE ";
             MessageBox.Show(tiles, "Your Tiles:");
-            while (tiles.IndexOf(" ") != -1)
+            while (tiles.IndexOf(" ") != -1)//Check each index of the string for a blank space, remove it and count it as a blank tile
             {
                 tiles = tiles.Remove(tiles.IndexOf(" "), 1);
             }
             int blankTiles = 7 - tiles.Length;
-            System.Net.WebClient wc = new System.Net.WebClient();
-            Stream download = wc.OpenRead("http://darcy.rsgc.on.ca/ACES/ICS4U/SourceCode/Words.txt");
-            StreamReader sr = new StreamReader(download);
-            StreamWriter sw = new StreamWriter("scoringWords.txt");
-            for (int i = 0; i < tiles.Length; i++)
+            try
             {
-                int removeLetter = badLetters.IndexOf(tiles.Substring(i, 1));
-                if (removeLetter != -1)
+                System.Net.WebClient wc = new System.Net.WebClient();//Read the dictionary
+                Stream download = wc.OpenRead("http://darcy.rsgc.on.ca/ACES/ICS4U/SourceCode/Words.txt");
+                StreamReader sr = new StreamReader(download);
+                StreamWriter sw = new StreamWriter("scoringWords.txt");
+                for (int i = 0; i < tiles.Length; i++)//Take out the tiles from the alphabet
                 {
-                    badLetters = badLetters.Remove(removeLetter, 1);
-                }
-            }
-            while (!sr.EndOfStream)
-            {
-                int counter = 0;
-                currentWord = sr.ReadLine().ToUpper();
-                tempWord = currentWord;
-                if (currentWord.Length < 8 && currentWord != lastWord)
-                {
-                    for (int i = 0; i < badLetters.Length; i++)
+                    int removeLetter = badLetters.IndexOf(tiles.Substring(i, 1));
+                    if (removeLetter != -1)
                     {
-                        if (currentWord.Contains(badLetters.Substring(i, 1)))
-                        {
-                            counter++;
-                        }
-                        if (counter > blankTiles)
-                        {
-                            i = 26;
-                        }
+                        badLetters = badLetters.Remove(removeLetter, 1);
                     }
-                    if (counter <= blankTiles)
+                }
+                while (!sr.EndOfStream)//Read through the whole stream
+                {
+                    scoringLetters = "";
+                    int counter = 0;
+                    currentWord = sr.ReadLine().ToUpper();
+                    tempWord = currentWord;
+
+                    //Logic sorted from most vague to most specific
+
+                    if (currentWord.Length < 8 && currentWord != lastWord)//Only read words if they are 7 or less characters and if they are not duplicates (in the case of the headers)
                     {
-                        for (int i = 0; i < tiles.Length; i++)
+                        for (int i = 0; i < badLetters.Length; i++)//Check if the current word contains any of the restricted letters
                         {
-                            string currentLetter = tiles.Substring(i, 1);
-                            int removeLetter = tempWord.IndexOf(currentLetter);
-                            if (removeLetter > -1)
+                            if (currentWord.Contains(badLetters.Substring(i, 1)))
                             {
-                                tempWord = tempWord.Remove(removeLetter, 1);
+                                counter++;//Count the number of restricted letters
+                            }
+                            if (counter > blankTiles)//If there are too many restricted letters
+                            {
+                                i = 26;//Cut the loop short to save time
                             }
                         }
-                        if (tempWord.Length == blankTiles || tempWord.Length == 0)
+                        if (counter <= blankTiles)//If the # of restricted letters is less than or equal to the # of blank tiles continue selection, in the case that there are no blank tiles there can be no restricted letters
                         {
-                            sw.WriteLine(currentWord);
-                            lastWord = currentWord;
+                            for (int i = 0; i < tiles.Length; i++)//For each tile
+                            {
+                                string currentLetter = tiles.Substring(i, 1);
+                                int removeLetter = tempWord.IndexOf(currentLetter);//Get the first index of the letter in the word
+                                if (removeLetter > -1)
+                                {
+                                    scoringLetters += tempWord.Substring(removeLetter, 1);//Record the valid letters to be scored
+                                    tempWord = tempWord.Remove(removeLetter, 1);//Take the tile out of the word
+                                }
+                            }
+                            if (tempWord.Length <= blankTiles)//If the number of letters left over is less than or equal to the # of blank tiles they are replaced with blank tiles, if there are no blank tiles at this point, length should be zero
+                            {
+                                lastWord = currentWord;//Set the last word to the current word to eliminate repetition of words
+                                tempString += currentWord + " ";
+                                int totalPoints = 0;
+                                for (int i = 0; i < scoringLetters.Length; i++)//Read each character of the string
+                                {
+                                        ScrabbleLetter sl = new ScrabbleLetter(scoringLetters.ToCharArray()[i]);//Get the point value for the character
+                                        totalPoints += sl.Points;//Increase the total points of the word
+                                }
+                                if (totalPoints > maxPoints)//If the current word has more points
+                                {
+                                    maxPoints = totalPoints;//Set best word and best points to the current values
+                                    topWord = currentWord.Substring(0, 1) + currentWord.Substring(1).ToLower();
+                                }
+                                else if (totalPoints == maxPoints)//If the points are the same 
+                                {
+                                    topWord += " or " + currentWord.Substring(0, 1) + currentWord.Substring(1).ToLower();//Add a new option for best points and best word 
+                                }
+                            }
                         }
                     }
                 }
+                sr.Close();
+                sw.Close();
+                MessageBox.Show(tempString + "\n\nHigh Scoring Word is " + topWord + " with a score of " + maxPoints + "\n\n" + "Your Tiles:\n\n" + tiles, "Scoring Words");//Print the output
             }
-            sr.Close();
-            sw.Close();
-            StreamReader sr2 = new StreamReader("scoringWords.txt");
-            string tempString = "";
-            int maxPoints = 0;
-            string topWord = "";
-            while (!sr2.EndOfStream)
+            catch (FileNotFoundException ex)
             {
-                currentWord = sr2.ReadLine();
-                int totalPoints = 0;
-                for (int i = 0; i < currentWord.Length; i++)
-                {
-                    if (!badLetters.Contains(currentWord.ToCharArray()[i]))
-                    {
-                        ScrabbleLetter sl = new ScrabbleLetter(currentWord.ToCharArray()[i]);
-                        totalPoints += sl.Points;
-                    }
-                }
-                if (totalPoints > maxPoints)
-                {
-                    maxPoints = totalPoints;
-                    topWord = currentWord.Substring(0, 1) + currentWord.Substring(1).ToLower();
-                }
-                else if (totalPoints == maxPoints)
-                {
-                    topWord += " or " + currentWord.Substring(0, 1) + currentWord.Substring(1).ToLower();
-                }
-                tempString += currentWord + " ";
+                MessageBox.Show("Error Encountered\n" + ex);//Account for errors
             }
-            MessageBox.Show(tempString + "\n\nHigh Scoring Word is " + topWord + " with a score of " + maxPoints + "\n\n" + "Your Tiles:\n\n" + tiles, "Scoring Words");
-            sr2.Close();
         }
     }
 }
--- ScrabbleGame.cs
+++ ScrabbleGame.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Name: Riley de Gans
+ * Date: April 22nd, 2019
+ * Description:  A program that cheats at scrabble using an online dictionary and some inefficient logic.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
